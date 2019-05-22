e56346ab22df564879032d1cffa87b3dcc8cab9a
2colours <37218286+2colours@users.noreply.github.com>
2019-05-22 06:42:29 +0200

    admin view basics

---
+++ MTR2.Web/ViewComponents/RepoArticleBigListViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTR2.Dal.Dtos;
+using MTR2.Dal.Services;
+using MTR2.Dal.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MTR2.Web.ViewComponents
+{
+	public class RepoArticleBigListViewComponent : ViewComponent
+	{
+		public class RepoArticleBigListModel
+		{
+			public IEnumerable<RepoArticleDto> RepoArticles { get; set; }
+			public bool IsAdmin { get; set; }
+		}
+		public RepoArticleBigListViewComponent(RepoArticleService repoArticleService)
+		{
+			RepoArticleService = repoArticleService;
+		}
+
+		public RepoArticleService RepoArticleService { get; }
+		public IViewComponentResult Invoke(ClaimsPrincipal user)
+		{
+			return View(new RepoArticleBigListModel
+			{
+				RepoArticles = RepoArticleService.GetRepoArticles(),
+				IsAdmin=user.IsInRole(Roles.Administrators)
+			});
+		}
+	}
+}
