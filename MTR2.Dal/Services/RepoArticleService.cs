dfccb24831343a6d44dd54ca9010b4d485eaa098
2colours <37218286+2colours@users.noreply.github.com>
2019-05-22 02:46:08 +0200

    RArticle view first steps

--- MTR2.Dal/Services/RepoArticleService.cs
+++ MTR2.Dal/Services/RepoArticleService.cs
@@ -1,7 +1,9 @@
 ﻿using MTR2.Dal.Dtos;
+using MTR2.Dal.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace MTR2.Dal.Services
@@ -13,12 +15,14 @@ namespace MTR2.Dal.Services
 			DbContext = dbContext;
 		}
 		public MTR2DbContext DbContext { get; }
-		public IEnumerable<RepoArticleDto> GetRepoArticles() => DbContext.RepoArticles.Select(r => new RepoArticleDto
+		public static Expression<Func<RepoArticle, RepoArticleDto>> RepoArticleSelector { get; } = r => new RepoArticleDto
 		{
 			Content = r.Content,
 			Id = r.Id,
 			Order = r.Order,
 			Title = r.Title
-		}).OrderBy(r => r.Order);
+		};
+		public IEnumerable<RepoArticleDto> GetRepoArticles() => DbContext.RepoArticles.Select(RepoArticleSelector).OrderBy(r => r.Order);
+		public RepoArticleDto GetRepoArticle(int id) => DbContext.RepoArticles.Where(r => r.Id == id).Select(RepoArticleSelector).FirstOrDefault();
 	}
 }
--- MTR2.Web/Pages/RArticle.cshtml.cs
+++ MTR2.Web/Pages/RArticle.cshtml.cs
@@ -2,16 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MTR2.Dal.Dtos;
+using MTR2.Dal.Entities;
+using MTR2.Dal.Services;
 
 namespace MTR2.Web.Pages
 {
     public class RArticleModel : PageModel
     {
-        public void OnGet()
-        {
+		public RArticleModel(RepoArticleService repoArticleService, UserManager<User> userManager)
+		{
+			RepoArticleService = repoArticleService;
+			UserManager = userManager;
+		}
+		public RepoArticleService RepoArticleService { get; }
+		public UserManager<User> UserManager { get; }
+		public RepoArticleDto RepoArticle { get; private set; }
+		private int? currentUserId;
+		public int? CurrentUserId =>
+		User.Identity.IsAuthenticated ?
+		(currentUserId ?? (currentUserId = int.Parse(UserManager.GetUserId(User)))) : null;
+		public ActionResult OnGet(int? id)
+		{
+			if (id == null)
+				return RedirectToPage("/Repo");
+			RepoArticle = RepoArticleService.GetRepoArticle(id.Value);
 
-        }
-    }
+			if (RepoArticle == null)
+				return NotFound();
+			return Page();
+		}
+		
+
+	}
 }
