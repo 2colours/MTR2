0b18c29aeb0e298f4f88f01cc4e4769a9f2d4765
2colours <37218286+2colours@users.noreply.github.com>
2019-05-22 19:37:45 +0200

    Edit Article tiny steps

--- MTR2.Web/Pages/EditRepoArticle.cshtml.cs
+++ MTR2.Web/Pages/EditRepoArticle.cshtml.cs
@@ -6,6 +6,7 @@ using System.Security.Principal;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MTR2.Dal.Dtos;
 using MTR2.Dal.Services;
 using MTR2.Dal.Users;
 
@@ -18,10 +19,12 @@ namespace MTR2.Web.Pages
 			RepoArticleService = repoArticleService;
 		}
 		public RepoArticleService RepoArticleService { get; }
+		public RepoArticleDto RepoArticle { get; private set; }
 		public ActionResult OnGet(int id)
 		{
 			if (!PageContext.HttpContext.User.IsInRole(Roles.Administrators))
 				return RedirectToPage("/Repo");
+			RepoArticle = RepoArticleService.GetRepoArticle(id);
 			return Page();
 
 		}
