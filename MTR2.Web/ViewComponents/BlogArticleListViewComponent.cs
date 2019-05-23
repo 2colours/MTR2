8f9c5fe4d687d6c523cc0e95cc5f2e2aae2d891c
2colours <37218286+2colours@users.noreply.github.com>
2019-05-23 04:01:36 +0200

    Let's try Posts

---
+++ MTR2.Web/ViewComponents/BlogArticleListViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTR2.Dal.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTR2.Web.ViewComponents
+{
+	public class BlogArticleListViewComponent : ViewComponent
+	{
+		public BlogArticleListViewComponent(BlogArticleService blogArticleService)
+		{
+			BlogArticleService = blogArticleService;
+		}
+
+		public BlogArticleService BlogArticleService { get; }
+
+		public IViewComponentResult Invoke()
+		{
+			return View(BlogArticleService.GetBlogArticles());
+		}
+	}
+}
